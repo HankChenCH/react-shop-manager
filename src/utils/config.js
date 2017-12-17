eaca11b48503c3fcf6e1ac2e27d99d0c779e88cd
Hank Chen <onegledog@163.com>
2017-12-17 20:54:27 +0800

    修改ws接口地址

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,7 @@ module.exports = {
   logo: '/st.jpg',
   iconFontCSS: '/iconfont.css',
   iconFontJS: '/iconfont.js',
-  websocketURL: true ? 'ws://127.0.0.1:9502' : 'wss://ws.zsshitan.com',
+  websocketURL: false ? 'ws://127.0.0.1:9502' : 'wss://ws.zsshitan.com',
   baseURL: false ? 'http://api.c.cn/api/v1' : 'https://api.zsshitan.com/api/v1',
   YQL: ['http://www.zuimeitianqi.com'],
   CORS: ['https://api.zsshitan.com/api/vi'],
